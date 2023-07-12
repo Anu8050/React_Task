ed4d2ddcc179fb53cb4f475c0886e54695c6219a
Ausha M S <anusgowda050@gmail.com>
2023-07-12 18:19:32 +0530

    #1
    If user name or password fields are empty when we click on login button it give error message. -> task completed.

--- my-react-app/src/Page/loginpage.js
+++ my-react-app/src/Page/loginpage.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
-import {Button, TextField} from '@mui/material';
+import {Button, TextField, Card, CardContent} from '@mui/material';
 function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [loginSuccess, setLoginSuccess] = useState(false);
+    const [fieldError, setFieldError] = useState(false);
+
     const handleClick = () => {
-        const user = database.find((user) => user.username === username && user.password === password);
-        if (user) {
-          setLoginSuccess(true);
-          setUsername('')
-          setPassword('')
-        } else {
-          setLoginSuccess(false);
-          alert('Invalid username or password');
-        }
-        setUsername('')
-        setPassword('')
+
+        if (username.trim() === '' || password.trim() === '') {
+            setFieldError(true);
+            return;
+          }
+      
+          const user = database.find((user) => user.username === username && user.password === password);
+          if (user) {
+            setLoginSuccess(true);
+            setUsername('');
+            setPassword('');
+          } else {
+            setLoginSuccess(false);
+            alert('Invalid username or password');
+          }
     };
     
     const database = [
@@ -26,16 +32,27 @@ function Login() {
 
     
     return (
-        <div>
-            <br/>
-            <TextField id="outlined-basic" label="Username" variant="outlined" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <div style={{marginTop:'-100px', background: 'url(https://img.freepik.com/free-photo/wide-angle-shot-singletree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg)', backgroundSize: 'cover', height: '120vh',alignItems: 'center', justifyContent: 'center', display: 'flex'}}>
+        <Card style={{alignItems: 'center', maxWidth: 300, margin: '0 auto', marginTop: '100px' }}>
+            <CardContent>
+            <TextField id="outlined-basic" label="Username" variant="outlined" value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+            error={fieldError && username.trim() === ''}
+            helperText={fieldError && username.trim() === '' ? 'Please enter the username' : ''}
+            />
             <br />
             <br/>
-            <TextField id="outlined-basic" label="Password" variant="outlined" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <TextField id="outlined-basic" label="Password" variant="outlined" value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            error={fieldError && password.trim() === ''}
+            helperText={fieldError && password.trim() === '' ? 'Please enter the password' : ''}
+            />
             <br/>
             <br/>
             <Button variant="contained" onClick={handleClick }> Login </Button>
             {loginSuccess && <p>Login Successful!</p>}
+            </CardContent>
+        </Card>
         </div>
 
       
